3e810777fe9f0c5f5e50882f05e31a20cc7cbce4
galta95 <galtaebi@gmail.com>
2022-05-12 18:56:15 +0300

    added vcomments page

---
+++ apps/focus-frontend/src/components/vcomment-page/VcommentPage.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { DELETEcomment, POSTcomment } from '../../common/api/video';
+import { useUser } from '../../common/hooks/users';
+import { useVideo } from '../../common/hooks/videos';
+import Video from '../video/Video';
+
+const VcommentPost = () => {
+    const { id: videoId } = useParams();
+    const [video, setVideo] = useVideo(videoId);
+    const [user] = useUser(video?.userId);
+
+    return (
+        <div>
+            <div className='mb-3'>
+                <Video video={video} properties={{ clickable: false, controls: true }} />
+            </div>
+            <div>
+                <Button>arrow</Button>
+                <Button>circle</Button>
+                <Button>squre</Button>
+            </div>
+        </div>
+    );
+};
+
+export default VcommentPost;
