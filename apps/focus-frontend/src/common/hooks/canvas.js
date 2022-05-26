b83f32566da2296a9a47e46ed9ea7531604e8966
yuval <artziyuval@gmail.com>
2022-05-26 11:46:17 +0300

    WIP: canvas with options

--- apps/focus-frontend/src/common/hooks/canvas.js
+++ apps/focus-frontend/src/common/hooks/canvas.js
@@ -1,29 +1,25 @@
 import { useRef, useEffect } from 'react'
 
-const useCanvas = draw => {
+const useCanvas = (draw, options={}) => {
   
   const canvasRef = useRef(null)
   
   useEffect(() => {
     
     const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
+    const context = canvas.getContext(options.context || '2d')
     let frameCount = 0
     let animationFrameId
-    
     const render = () => {
       frameCount++
       draw(context, frameCount)
       animationFrameId = window.requestAnimationFrame(render)
     }
     render()
-    
     return () => {
       window.cancelAnimationFrame(animationFrameId)
     }
   }, [draw])
-  
   return canvasRef
 }
-
 export default useCanvas
--- apps/focus-frontend/src/components/canvas/Canvas.jsx
+++ apps/focus-frontend/src/components/canvas/Canvas.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import useCanvas from '../../common/hooks/canvas';
 
 const Canvas = (props) => {
-    const { draw, ...rest } = props;
-    const canvasRef = useCanvas(draw);
+    const { draw, options, ...rest } = props;
+    const { context, ...moreConfig } = options;
+    const canvasRef = useCanvas(draw, { context });
 
     return <canvas ref={canvasRef} {...rest} />;
 };
