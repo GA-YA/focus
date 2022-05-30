b7bdd9ddc9c1796f65101e8bed34dd752c698210
yuval <artziyuval@gmail.com>
2022-05-30 20:57:29 +0300

    added buttons to add circle or rectangle

--- apps/focus-frontend/src/components/vcomment-page/VcommentPage.jsx
+++ apps/focus-frontend/src/components/vcomment-page/VcommentPage.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import { DELETEcomment, POSTcomment } from '../../common/api/video';
 import { useUser } from '../../common/hooks/users';
 import { useVideo } from '../../common/hooks/videos';
 import Video from '../video/Video';
@@ -11,28 +10,57 @@ const VcommentPost = () => {
     const { id: videoId } = useParams();
     const [video, setVideo] = useVideo(videoId);
     const [user] = useUser(video?.userId);
+    const [shapes, setShapes] = useState([]);
 
-    const draw = (ctx, frameCount) => {
+    function clearDraw(ctx) {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.fillStyle = '#000000';
+    }
+
+    function drawRect(ctx, frameCount) {
+        clearDraw(ctx);
         ctx.beginPath();
-        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-        ctx.fill();
+        ctx.rect(20, 20, 50, 50);
+        ctx.stroke();
+    }
+
+    const drawCircle = (ctx, frameCount) => {
+        clearDraw(ctx);
+        ctx.beginPath();
+        ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+        ctx.stroke();
     };
+
+    function addRect() {
+        const newShape = shapes.concat('rect');
+        setShapes(newShape);
+    }
+
+    function addCircle() {
+        const newShape = shapes.concat('circle');
+        setShapes(newShape);
+    }
 
     return (
         <div>
-            <div>hi</div>
-            <div>
-                <Canvas draw={draw} />
+            <div className='mb-3'>
+                <div className='overflow-scroll' style={{ height: '400px' }}>
+                    {shapes.map((draw) => {
+                        if (draw === 'rect') {
+                            return <Canvas draw={drawRect} />;
+                        }
+                        if (draw === 'circle') {
+                            return <Canvas draw={drawCircle} />;
+                        }
+                    })}
+                </div>
+                <div>
+                    <Button onClick={addRect}>add rect</Button>
+                    <Button onClick={addCircle}>add circle</Button>
+                </div>
             </div>
+
             <div className='mb-3'>
                 <Video video={video} properties={{ clickable: false, controls: true }} />
-            </div>
-            <div>
-                <Button>arrow</Button>
-                <Button>circle</Button>
-                <Button>squre</Button>
             </div>
         </div>
     );
